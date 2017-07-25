bb11b64b57f85f07f5c35769616163ef0a95b04b
yu-139k-ta <dreams.0713@gmail.com>
2017-07-25 16:21:56 +0900

    teamのルーティングを作成

---
+++ src/AppBundle/Controller/TeamController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/AppBundle/Controller/teamController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class TeamController extends Controller
+{
+    /**
+     * @Route("/team")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
