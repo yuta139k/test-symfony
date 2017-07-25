eeccd79ae2690a4c93334b9cc46db13584f5fa9a
yu-139k-ta <dreams.0713@gmail.com>
2017-07-25 14:36:35 +0900

    FirstControllerの作成

---
+++ src/AppBundle/Controller/FirstController.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FirstController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
