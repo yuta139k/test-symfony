fa94007ec145a857a3e4290083c53b5da66ff144
yu-139k-ta <dreams.0713@gmail.com>
2017-08-03 16:55:30 +0900

    打率計算結果の小数点第三位以下は切り捨てとなるテストを追加

--- tests/AppBundle/Service/PlayerGameScoreCalculateServiceTest.php
+++ tests/AppBundle/Service/PlayerGameScoreCalculateServiceTest.php
@@ -16,4 +16,14 @@ class PlayerGameScoreCalculateServiceTest extends TestCase
 
         $this->assertSame(0.200, $service->calcBattingAverage($score));
     }
+
+    public function test小数点第三位以下の打率は切り捨てになる()
+    {
+        $service = new PlayerGameScoreCalculateService();
+        $score = new PlayerGameScore();
+        $score->setAtBats(10);
+        $score->setHits(3);
+
+        $this->assertSame(0.333, $service->calcBattingAverage($score));
+    }
 }
