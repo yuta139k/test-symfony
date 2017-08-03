bf263ac552a05364f31c8ce6bfed004b72f47429
yu-139k-ta <dreams.0713@gmail.com>
2017-08-03 16:32:09 +0900

    打率を計算する機能のテストを作成

--- tests/AppBundle/Service/PlayerGameScoreCalculateServiceTest.php
+++ tests/AppBundle/Service/PlayerGameScoreCalculateServiceTest.php
@@ -2,8 +2,18 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Entity\PlayerGameScore;
 use PHPUnit\Framework\TestCase;
 
 class PlayerGameScoreCalculateServiceTest extends TestCase
 {
+    public function test打率が計算できる()
+    {
+        $service = new PlayerGameScoreCalculateService();
+        $score = new PlayerGameScore();
+        $score->setAtBats(10);
+        $score->setHits(2);
+
+        $this->assertEquals('0.200', $service->calcBattingAverage($score));
+    }
 }
