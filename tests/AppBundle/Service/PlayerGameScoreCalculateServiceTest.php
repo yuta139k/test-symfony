f5bd774c71ebd875eec6894387cb5c141fd66fbb
yu-139k-ta <dreams.0713@gmail.com>
2017-08-04 22:45:11 +0900

    打率が10割以上となる場合は不正とし例外発生するテストを追加

--- tests/AppBundle/Service/PlayerGameScoreCalculateServiceTest.php
+++ tests/AppBundle/Service/PlayerGameScoreCalculateServiceTest.php
@@ -20,11 +20,32 @@ class PlayerGameScoreCalculateServiceTest extends TestCase
         $this->assertSame($expected, $service->calcBattingAverage($score));
     }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     * @dataProvider invalidBatsAndHits
+     */
+    public function test打率10割以上は不正とし例外発生($bats, $hits)
+    {
+        $service = new PlayerGameScoreCalculateService();
+        $score = new PlayerGameScore();
+        $score->setAtBats($bats);
+        $score->setHits($hits);
+
+        $service->calcBattingAverage($score);
+    }
+
     public function batsAndHits()
     {
         return [
             [3, 1, 0.333],
             [10, 2, 0.200],
+        ];
+    }
+
+    public function invalidBatsAndHits()
+    {
+        return [
+            [3, 10],
         ];
     }
 }
